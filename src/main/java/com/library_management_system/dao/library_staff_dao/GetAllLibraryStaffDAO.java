b3fa77989d7d697b32c70dce75cb6218165b33bd
AdityaKalambe <adityakalambe20@gmail.com>
2023-12-17 23:35:14 +0530

    Get All Library Staff DAO

---
+++ src/main/java/com/library_management_system/dao/library_staff_dao/GetAllLibraryStaffDAO.java
@@ -0,0 +1,19 @@
+package com.library_management_system.dao.library_staff_dao;
+
+import com.library_management_system.entity.LibraryStaff;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class GetAllLibraryStaffDAO {
+    @Autowired
+    SessionFactory sessionFactory;
+    public List<LibraryStaff> getAllLibraryStaff(){
+        return sessionFactory.openSession()
+                .createCriteria(LibraryStaff.class)
+                .list();
+    }
+}
