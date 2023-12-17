9fb266c0731885accc8afc14f594a7b572183073
AdityaKalambe <adityakalambe20@gmail.com>
2023-12-17 13:32:32 +0530

    Search Book By Author Dao

---
+++ src/main/java/com/library_management_system/dao/book/SearchBookByAuthorDAO.java
@@ -0,0 +1,21 @@
+package com.library_management_system.dao.book;
+
+import com.library_management_system.entity.Book;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class SearchBookByAuthorDAO {
+    @Autowired
+    SessionFactory sessionFactory;
+    public List<Book> searchBookByAuthor(String authorName){
+        return sessionFactory.openSession()
+                .createCriteria(Book.class)
+                .add(Restrictions.like("bookAuthor",authorName))
+                .list();
+    }
+}
