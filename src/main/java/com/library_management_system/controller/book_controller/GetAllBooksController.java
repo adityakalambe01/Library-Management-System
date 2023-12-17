62fe66cb94a8fbfa49ec14b3c0a49325bf516313
AdityaKalambe <adityakalambe20@gmail.com>
2023-12-17 19:07:22 +0530

    All Books Controller

---
+++ src/main/java/com/library_management_system/controller/book_controller/GetAllBooksController.java
@@ -0,0 +1,20 @@
+package com.library_management_system.controller.book_controller;
+
+import com.library_management_system.entity.Book;
+import com.library_management_system.service.book_services.GetAllBooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GetAllBooksController {
+    @Autowired
+    GetAllBooksService getAllBooksService;
+    @RequestMapping(value = "/getAllBooks",method = RequestMethod.GET)
+    public List<Book> getAllBooks(){
+        return getAllBooksService.getAllBooks();
+    }
+}
