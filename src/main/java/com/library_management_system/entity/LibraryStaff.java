529ba34973f22206090f1b7cdce396c6454af2b9
AdityaKalambe <adityakalambe20@gmail.com>
2023-12-17 23:04:15 +0530

    Staff Entity

--- src/main/java/com/library_management_system/entity/LibraryStaff.java
+++ src/main/java/com/library_management_system/entity/LibraryStaff.java
@@ -21,4 +21,72 @@ public class LibraryStaff {
     private String staffMemberPassword;
 
 
+    public LibraryStaff() {
+    }
+
+    public LibraryStaff(Long staffMemberID, String staffMemberName, String staffMemberPosition, Long staffMemberContactNumber, String staffMemberPassword) {
+        this.staffMemberID = staffMemberID;
+        this.staffMemberName = staffMemberName;
+        this.staffMemberPosition = staffMemberPosition;
+        this.staffMemberContactNumber = staffMemberContactNumber;
+        this.staffMemberPassword = staffMemberPassword;
+    }
+
+    public LibraryStaff(String staffMemberName, String staffMemberPosition, Long staffMemberContactNumber, String staffMemberPassword) {
+        this.staffMemberName = staffMemberName;
+        this.staffMemberPosition = staffMemberPosition;
+        this.staffMemberContactNumber = staffMemberContactNumber;
+        this.staffMemberPassword = staffMemberPassword;
+    }
+
+    public Long getStaffMemberID() {
+        return staffMemberID;
+    }
+
+    public void setStaffMemberID(Long staffMemberID) {
+        this.staffMemberID = staffMemberID;
+    }
+
+    public String getStaffMemberName() {
+        return staffMemberName;
+    }
+
+    public void setStaffMemberName(String staffMemberName) {
+        this.staffMemberName = staffMemberName;
+    }
+
+    public String getStaffMemberPosition() {
+        return staffMemberPosition;
+    }
+
+    public void setStaffMemberPosition(String staffMemberPosition) {
+        this.staffMemberPosition = staffMemberPosition;
+    }
+
+    public Long getStaffMemberContactNumber() {
+        return staffMemberContactNumber;
+    }
+
+    public void setStaffMemberContactNumber(Long staffMemberContactNumber) {
+        this.staffMemberContactNumber = staffMemberContactNumber;
+    }
+
+    public String getStaffMemberPassword() {
+        return staffMemberPassword;
+    }
+
+    public void setStaffMemberPassword(String staffMemberPassword) {
+        this.staffMemberPassword = staffMemberPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryStaff{" +
+                "staffMemberID=" + staffMemberID +
+                ", staffMemberName='" + staffMemberName + '\'' +
+                ", staffMemberPosition='" + staffMemberPosition + '\'' +
+                ", staffMemberContactNumber=" + staffMemberContactNumber +
+                ", staffMemberPassword='" + staffMemberPassword + '\'' +
+                '}';
+    }
 }
