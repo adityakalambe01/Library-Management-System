e8877bf24f3c04f246eb8c33d00c36fa71d1a4a7
AdityaKalambe <adityakalambe20@gmail.com>
2023-12-17 23:00:34 +0530

    Add Member Controller

---
+++ src/main/java/com/library_management_system/controller/member_controller/AddMemberController.java
@@ -0,0 +1,19 @@
+package com.library_management_system.controller.member_controller;
+
+import com.library_management_system.entity.Member;
+import com.library_management_system.service.member_services.AddMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddMemberController {
+    @Autowired
+    AddMemberService addMemberService;
+    @RequestMapping(value = "addMember",method = RequestMethod.POST)
+    public Boolean addMember(@RequestBody Member member){
+        return addMemberService.addMember(member);
+    }
+}
